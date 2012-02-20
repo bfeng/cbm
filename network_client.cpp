91310406ada03c340a3fbc8c1b08aa54630271c5
Bo Feng <bfeng1216@gmail.com>
2012-02-20 00:55:32 -0600

    incomplete tcp testing

--- network_client.cpp
+++ network_client.cpp
@@ -1,8 +1,24 @@
 #include "stdafx.hpp"
 
 using boost::asio::ip::udp;
+using boost::asio::ip::tcp;
 
-void client_worker(char host[], char port[], const unsigned int buf_size, long *size, double *dur)
+void print_result(long size[], double dur[], int n_thread)
+{
+  long t_size = 0;
+  double t_dur = 0.0;
+  for(int i=0;i<n_thread;++i)
+  {
+    t_size += size[i];
+    t_dur += dur[i];
+  }
+
+  std::cout << "Time: " << t_dur << " s" << std::endl;
+  std::cout << "Size: " << t_size << "B" << std::endl;
+  std::cout << "Speed: " << ((double)t_size)*8/1024/1024/(t_dur) << "Mbps" << std::endl;
+}
+
+void udp_client_worker(char host[], char port[], const unsigned int buf_size, long *size, double *dur)
 {
   boost::asio::io_service io_service;
 
@@ -36,7 +52,7 @@ void client_worker(char host[], char port[], const unsigned int buf_size, long *
   }
 }
 
-void client(char host[], char port[], const unsigned int buf_size, int n_thread)
+void udp_client(char host[], char port[], const unsigned int buf_size, int n_thread)
 {
   try
   {
@@ -44,23 +60,12 @@ void client(char host[], char port[], const unsigned int buf_size, int n_thread)
     double dur[n_thread];
     boost::thread worker[n_thread];
     for(int i=0;i<n_thread;++i)
-      worker[i] = boost::thread(client_worker, host, port, buf_size, &size[i], &dur[i]);
+      worker[i] = boost::thread(udp_client_worker, host, port, buf_size, &size[i], &dur[i]);
 
     for(int i=0;i<n_thread;++i)
       worker[i].join();
 
-    long t_size = 0;
-    double t_dur = 0.0;
-
-    for(int i=0;i<n_thread;++i)
-    {
-      t_size += size[i];
-      t_dur += dur[i];
-    }
-
-    std::cout << "Time: " << t_dur << " s" << std::endl;
-    std::cout << "Size: " << t_size << "B" << std::endl;
-    std::cout << "Speed: " << ((double)t_size)*8/1024/1024/(t_dur) << "Mbps" << std::endl;
+    print_result(size, dur, n_thread);
   }
   catch (std::exception& e)
   {
@@ -68,16 +73,77 @@ void client(char host[], char port[], const unsigned int buf_size, int n_thread)
   }
 }
 
+void tcp_client_worker(char host[], char port[], const unsigned int buf_size, long *size, double *dur)
+{
+  boost::asio::io_service io_service;
+
+  tcp::resolver resolver(io_service);
+  tcp::resolver::query query(tcp::v4(), host, port);
+
+  tcp::resolver::iterator iterator = resolver.resolve(query);
+  tcp::socket socket(io_service);
+  socket.connect(*iterator);
+
+  int times = 100*1024/buf_size + ((100*1024%buf_size==0)?0:1);
+
+  *size = 0;
+  *dur = 0.0;
+
+  for(int i=0;i<times;++i)
+  {
+    char arr[buf_size];
+    cbm::rand_char_array(arr, buf_size);
+    const char * request = arr;
+
+    auto start = NOW();
+    boost::asio::write(socket, boost::asio::buffer(request, buf_size));
+
+    char recv_buf[buf_size];
+    size_t len = boost::asio::read(socket, boost::asio::buffer(recv_buf, buf_size));
+
+    auto end = NOW();
+    *dur += DURATION(end, start);
+    *size += buf_size + len;
+  }
+}
+
+void tcp_client(char host[], char port[], const unsigned int buf_size, int n_thread)
+{
+  try
+  {
+    long size[n_thread];
+    double dur[n_thread];
+    boost::thread worker[n_thread];
+    for(int i=0;i<n_thread;++i)
+      worker[i] = boost::thread(tcp_client_worker, host, port, buf_size, &size[i], &dur[i]);
+
+    for(int i=0;i<n_thread;++i)
+      worker[i].join();
+
+    print_result(size, dur, n_thread);
+  }
+  catch (std::exception &e)
+  {
+    std::cerr << e.what() << std::endl;
+  }
+}
+
 int main(int argc, char * argv[])
 {
-  if (argc != 3)
+  if (argc != 6)
   {
-    std::cerr << "Usuage: <host> <port>" << std::endl;
+    std::cerr << "Usuage: <tcp/udp> <host> <port> <buf_size> <n_thread>" << std::endl;
     return 1;
   }
   srand(time(0));
-  for(int i=0;i<10;++i)
-    client(argv[1], argv[2], 1024, 4);
+  int buf_size = atoi(argv[4]);
+  int n_thread = atoi(argv[5]);
+  if(strcmp(argv[1], "udp")==0)
+    for(int i=0;i<10;++i)
+      udp_client(argv[2], argv[3], buf_size, n_thread);
+  else
+    for(int i=0;i<10;++i)
+      tcp_client(argv[2], argv[3], buf_size, n_thread);
   return 0;
 }
 
--- network_server.cpp
+++ network_server.cpp
@@ -1,27 +1,26 @@
 #include "stdafx.hpp"
 
 using boost::asio::ip::udp;
+using boost::asio::ip::tcp;
 
-void server_worker(udp::socket *socket)
+void udp_server_worker(udp::socket *socket, int buf_size)
 {
   for (;;)
   {
-    char recv_buf[128];
+    char recv_buf[buf_size];
     udp::endpoint remote_endpoint;
     boost::system::error_code error;
-    socket->receive_from(boost::asio::buffer(recv_buf), remote_endpoint, 0, error);
+    int recv_len = socket->receive_from(boost::asio::buffer(recv_buf, buf_size), remote_endpoint, 0, error);
 
     if (error && error != boost::asio::error::message_size)
       throw boost::system::system_error(error);
 
-    std::string message = "SVR RPL";
-
     boost::system::error_code ignored_error;
-    socket->send_to(boost::asio::buffer(message), remote_endpoint, 0, ignored_error);
+    socket->send_to(boost::asio::buffer(recv_buf, recv_len), remote_endpoint, 0, ignored_error);
   }
 }
 
-void server(int port, int n_thread)
+void udp_server(int port, int buf_size, int n_thread)
 {
   try
   {
@@ -30,7 +29,7 @@ void server(int port, int n_thread)
 
     boost::thread worker[n_thread];
     for(int i=0;i<n_thread;++i)
-      worker[i] = boost::thread(server_worker, &socket);
+      worker[i] = boost::thread(udp_server_worker, &socket, buf_size);
       
     for(int i=0;i<n_thread;++i)
       worker[i].join();
@@ -41,8 +40,63 @@ void server(int port, int n_thread)
   }
 }
 
-int main()
+void tcp_server_worker(tcp::acceptor *acceptor, int buf_size)
 {
-  server(13000, 4);
+  boost::asio::io_service io_service;
+  for(;;)
+  {
+    tcp::socket socket(io_service);
+    acceptor->accept(socket);
+
+    char recv_buf[buf_size];
+    boost::system::error_code error;
+    int recv_len = socket.read_some(boost::asio::buffer(recv_buf, buf_size), error);
+
+    if (error == boost::asio::error::eof)
+      break;
+    else if(error)
+      throw boost::system::system_error(error);
+
+
+    std::cout << recv_buf << std::endl;
+
+    boost::asio::write(socket, boost::asio::buffer(recv_buf, recv_len));
+  }
+}
+
+void tcp_server(int port, int buf_size, int n_thread)
+{
+  try
+  {
+    boost::asio::io_service io_service;
+    tcp::acceptor acceptor(io_service, tcp::endpoint(tcp::v4(), port));
+
+    boost::thread worker[n_thread];
+    for(int i=0;i<n_thread;++i)
+      worker[i] = boost::thread(tcp_server_worker, &acceptor, buf_size);
+
+    for(int i=0;i<n_thread;++i)
+      worker[i].join();
+  }
+  catch (std::exception & e)
+  {
+    std::cerr << e.what() << std::endl;
+  }
+}
+
+int main(int argc, char *argv[])
+{
+  if(argc!=5)
+  {
+    std::cerr << "Usuage: <tcp/udp> <port> <buf_size> <n_thread>" << std::endl;
+    return 1;
+  }
+  int port = atoi(argv[2]);
+  int buf_size = atoi(argv[3]);
+  int n_thread = atoi(argv[4]);
+  if(strcmp(argv[1], "udp")==0)
+    udp_server(port, buf_size, n_thread);
+  else
+    tcp_server(port, buf_size, n_thread);
   return 0;
 }
